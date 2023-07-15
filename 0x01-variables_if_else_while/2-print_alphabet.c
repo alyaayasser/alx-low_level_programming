1330f91d8899da640bb145d43f09fa7e54df2cac
alyaayasser <aliaayasser270@gmail.com>
2023-07-14 17:36:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using for loop to print the alphabet
+ *
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+char a;
+
+for (a = 'a' ; a <= 'z' ; a++)
+putchar(a);
+putchar('\n');
+	return (0);
+}
