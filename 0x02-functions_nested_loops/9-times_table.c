6fd8760164c8fbbbdd5f8f14d40416463b9ae433
alyaayasser <aliaayasser270@gmail.com>
2023-07-19 15:53:39 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,8 +12,8 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-
-	for (mult = 0; mult <= 9; mult++)
+		_putchar(48);
+	for (mult = 1; mult <= 9; mult++)
 	{
 		_putchar(',');
 		_putchar(' ');
