bca31e1b6af3bf3accf3a9fae7cc41ee99625fa2
alyaayasser <aliaayasser270@gmail.com>
2023-07-18 18:39:51 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_0x1 - function that will print the alphabet 10 times
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  *
  */
 void print_alphabet_x10(void)
