93c2856038e6e4f099bba9a3e8ba3ebabd5843ad
alyaayasser <aliaayasser270@gmail.com>
2023-07-19 15:08:59 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 9; numm++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
 	for (mult = 0; mult <= 9; mult++)
@@ -26,8 +26,8 @@ void times_table(void)
 		if (prod <= 9)
 			_putchar(' ');
 		else
-			_putchar((prod / 10) + 48)/*get the first digit*/
-				_putchar((prod % 10) + 48)/*get the second digit*/
+			_putchar((prod / 10) + 48); /*get the first digit*/
+				_putchar((prod % 10) + 48); /*get the second digit*/
 	}
 	_putchar('\n');
 	}
