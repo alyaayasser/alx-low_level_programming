b3bca777c366ae62aeb4e3bb1000f34ed410bdd2
alyaayasser <aliaayasser270@gmail.com>
2023-07-24 13:24:00 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - lmprlne an reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longl = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longl++;
+		s++;
+	}
+	s--;
+	for (o = longl; o > 0; o--)
+	{
+		_putchar(*s);
+		s--
+	}
+	_putchar('\n')
+}
