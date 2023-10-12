9c30a59dfb9f35e557096971e011383c5289e19d
alyaayasser <aliaayasser270@gmail.com>
2023-10-12 13:20:39 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_dlistint - prints doubly-linked list
+ * @h: address of head
+ *
+ * Return: size of list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
